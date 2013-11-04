0b2a72ce207a8bf569fd7f018fdf64b9b8489b4a
Carl Levasseur <levasseur.cl@gmail.com>
2013-11-04 23:16:28 +0100

    update tests

--- models/dataValidator.js
+++ models/dataValidator.js
@@ -2,24 +2,37 @@ function dataValidator(){}
 
 var singleton = new dataValidator();
 
-dataValidator.prototype.getMissingProperties = function(obj, requiredProps){
-	var missingProperties = [];
-	
-	if (!obj) {
-		missingProperties = requiredProps;
-	}
+/* NOT TESTED
+ *
+ *dataValidator.prototype.getMissingProperties = function(obj, requiredProps){
+ *        var missingProperties = [];
+ *        
+ *        if (!obj) {
+ *                missingProperties = requiredProps;
+ *        }
+ *
+ *        var p;
+ *        for (var i = 0; i < requiredProps.length; i++) {
+ *                p = requiredProps[i];
+ *                if (typeof obj[p] == 'undefined') {
+ *                        missingProperties.push(p);
+ *                }
+ *        }
+ *
+ *        return missingProperties;
+ *};
+ */
 
-	var p;
-	for (var i = 0; i < requiredProps.length; i++) {
-		p = requiredProps[i];
-		if (typeof obj[p] == 'undefined') {
-			missingProperties.push(p);
-		}
-	}
-
-	return missingProperties;
-};
-
+/**
+ *validate get required properties in an object literal that are missing and make sure that
+ *property of required values are valid following the validator provided for the property
+ *
+ * @param data An object literal to validate
+ * @param propertyValidators Contains required properties and possibly their respective
+ * validator, which must be a boolean function or null. If the validator function
+ * return true, the value is considered valid, invalid otherwise
+ * @return an object containing two array called missingProperties and invalidProperties
+ */
 dataValidator.prototype.validate = function(data, propertyValidators){
 	var err = {
 		missingProperties : [],
@@ -29,10 +42,26 @@ dataValidator.prototype.validate = function(data, propertyValidators){
 	for (var key in propertyValidators) {
 		if (propertyValidators.hasOwnProperty(key)) {
 			if (typeof data[key] == 'undefined') {
+				// Corresponding validator isn't check in this case
 				err.missingProperties.push(key);				
 			}
-			else if (propertyValidators[key](key) === false){
-				err.invalidProperties.push(key);
+			// Property validators must return a boolean
+			// if they are defined
+			else if (typeof propertyValidators[key] == 'function'){
+				var res = propertyValidators[key](key);
+				if (typeof res != 'boolean') {
+					throw new Error('Property validators must return a boolean');
+				}
+				if (res === false) {
+					err.invalidProperties.push(key);
+				}
+			}
+			// Property validators are either a function
+			// or null
+			else if (propertyValidators[key] !== null) {
+				throw new Error('Invalid property validator : ' 
+						+ propertyValidators[key] +
+						' (expected a function or null)');
 			}
 		}
 	};
@@ -40,9 +69,12 @@ dataValidator.prototype.validate = function(data, propertyValidators){
 	return err;
 };
 
-/*
- *A valid username contains between 3 and 25 alphanumeric characters
- *including the underscore
+/**
+ * validateUsername checks that the username contains between 
+ * 3 and 25 alphanumeric characters including the underscore
+ *
+ * @param username The username to test
+ * @return true is the username is valid, false otherwise
  */
 dataValidator.prototype.validateUsername = function(username) {
 	var reg = /^\w{3,25}$/;
@@ -52,17 +84,27 @@ dataValidator.prototype.validateUsername = function(username) {
 	return false;
 };
 
-/*
- *A valid password contains at least 8 non-blank characters
+/**
+ * validatePassword checks that the password contains at least 8 characters
+ * All characters allowed, except newline or line terminator
+ *
+ * @param password The password to test
+ * @return true is the password is valid, false otherwise
  */
 dataValidator.prototype.validatePassword = function(password) {
-	var reg = /^\S{8,}$/;
+	var reg = /^.{8,}$/;
 	if (reg.test(password)){
 		return true; 
 	}
 	return false;
 };
 
+/**
+ * validateEmail checks that the email is a correct email address
+ *
+ * @param email The address to verify
+ * @return true is the email is valid, false otherwise
+ */
 dataValidator.prototype.validateEmail= function(email) {
 	var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 	if (reg.test(email)){
--- test/dataValidator.js
+++ test/dataValidator.js
@@ -4,24 +4,71 @@ var util = require('util');
 var assert = require('assert');
 var dv = require('../models/dataValidator');
 
-describe('dataValidator.checkRequired', function(){
-	it('should return a rejected promise with an error containing all missing fields if any', function(){
-		(function(){
-			dv.checkRequired({ v1 : 'a', v2 : 'b', v3 : 'c' },
-					 ['v1', 'v2', 'v3', 'v4'])
-		}).should.throw();
+describe('dataValidator.validate', function(){
+	it('should return an error containing all missing/invalid fields if any', function(){
+		var err = dv.validate({
+			propA : 'valueA',
+			propB : 'valueB',
+			propC : 'valueC'
+		}, {
+			propA : function(){ return true; },
+			propB : function(){ return false; },
+			propC : null,
+			propD : null
+		});
+
+		err.should.eql({
+			missingProperties : ['propD'],
+			invalidProperties : ['propB']
+		});
 	});
 
-	it('should return a resolved promise if all fields are supplied', function(){
+	it('should throw an error if a property is present and has a validator which isn\'t a boolean function or null', function(){
 		(function(){
-			dv.checkRequired({ v1 : 'a', v2 : 'b', v3 : 'c', v4 : 'd' }, 
-					  ['v1', 'v2', 'v3', 'v4'])
-					 .should.not.throw();
+			dv.validate({
+				prop : 'value'
+			}, {
+				prop : function(){ return 1; }
+			});
+		}).should.throw();
+
+		(function(){
+			dv.validate({
+				prop : 'value'
+			}, {
+				prop : ''
+			});
 		}).should.throw();
 	});
 });
 
-describe.only('dataValidator.validateEmail', function(){
+describe('dataValidator.validateUsername', function(){
+	it('should return false if the username length is lower than 3 or higher than 25', function(){
+		dv.validateUsername('ab').should.be.false;
+		dv.validateUsername('thats1asuper2longusername3').should.be.false;
+	});
+
+	it('should return true if the username contains between 3 and 25 alphanumeric characters (including the underscore)', function(){
+		dv.validateUsername('123test9ing123').should.be.true;
+	});
+
+	it('should return false if the username contains forbidden characters', function(){
+		dv.validateUsername('John-Smith').should.be.false;
+		dv.validateUsername('John Smith').should.be.false;
+	});
+});
+
+describe('dataValidator.validatePassword', function(){
+	it('should return false if the password length is lower than 8', function(){
+		dv.validatePassword('short').should.be.false;
+	});
+
+	it('should return true if the password length is higher than 8 or equal', function(){
+		dv.validatePassword('abc 123!').should.be.true;
+	});
+});
+
+describe('dataValidator.validateEmail', function(){
 	it('should return false if email is invalid', function(){
 		dv.validateEmail('test');
 		dv.validateEmail('test@');
@@ -43,10 +90,3 @@ describe.only('dataValidator.validateEmail', function(){
 	});
 });
 
-describe('dataValidator.validateUsername', function(){
-
-});
-
-describe('dataValidator.checkPassword', function(){
-
-});

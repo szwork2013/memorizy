d84952ae41c59626b46dfc5326162cdbde404d16
Carl Levasseur <levasseur.cl@gmail.com>
2013-11-08 23:32:13 +0100

    register.createUser now pass all tests

--- test/db.js
+++ test/db.js
@@ -2,15 +2,23 @@ var should = require('should');
 var sinon = require('sinon');
 var util = require('util');
 var assert = require('assert');
+var pg = require('pg');
 
 var db = require('../models/db');
 
 describe('db.executePreparedStatement', function(){
 	it('should return a rejected promise if connection to the database fail', function(done){
-		var conn = db.conn;
-		db.conn = 'invalid database address';
-		db.executePreparedStatement('test').catch(function(){ done() });
-		db.conn = conn;
+		var stub = sinon.stub(pg, 'connect', function(conn, callback){
+			callback(new Error('Could not connect to the database'));
+		});
+
+		db.executePreparedStatement('select 1').then(function(){
+			done(new Error('db.executePreparedStatement should have returned a rejected promise'));	
+		}).catch(function(){ 
+			stub.calledOnce.should.be.true;
+			stub.restore();
+			done();
+		});
 	});
 
 	it('should return a rejected promise if the query is invalid', function(done){

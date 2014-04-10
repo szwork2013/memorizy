c3b0e130916fe0fa5c55b82b7423163ca2ede0a5
Carl Levasseur <levasseur.cl@gmail.com>
2014-04-10 18:19:54 +0200

    fixed undefined $http

--- public/js/login/LoginModel.js
+++ public/js/login/LoginModel.js
@@ -1,18 +1,17 @@
 angular.module('memorizy.login.LoginModel', [])
 .provider('LoginModel', function () {
   this.$get = ['$rootScope', '$http', '$location', '$localStorage',
-    function ($rootScope, $window, $http, $location, $localStorage) {
+    function ($rootScope, $http, $location, $localStorage) {
       return {
         login: function (username, password) {
           return $http.post('/login', { 
             username: username,
             password: password
-          }).success(function (data) {
+          })
+          .success(function (data) {
             $rootScope.user = data.user; 
             $localStorage.user = data.user;
             $localStorage.token = data.token;
-          })
-          .error(function (err, status, headers, config) {
           });
         }
       };

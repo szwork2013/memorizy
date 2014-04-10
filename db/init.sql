556510ee489f47a1c830445066cb12e44103c5a1
Carl Levasseur <levasseur.cl@gmail.com>
2014-04-10 22:01:01 +0200

    added sql commands to insert necessary data for the db to work

---
+++ db/init.sql
@@ -0,0 +1,22 @@
+-- Missing schema dump !
+
+-- Setup all plpgsql functions
+\i ./plpgsql/all.sql 
+
+delete from files;
+delete from users;
+
+insert into users (id, name, password, email, enabled) values (
+	0, 'root', 'root', 'root@memorizy', false);
+
+insert into files (id, owner_id, name, size, type) values (
+	0, 0, 'root', 0, 'folder');
+
+insert into file_tree (ancestor_id, descendant_id, dist) values ( 0, 0, 0 );
+
+-- insert study modes
+
+insert into study_modes (mode) values ('Classic');
+insert into study_modes (mode) values ('Hardest to easiest');
+insert into study_modes (mode) values ('Least studied');
+insert into study_modes (mode) values ('Wrongs');

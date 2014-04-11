528a627c4fbc8e6f2d0ab022fb9c34c316c5cdc1
Carl Levasseur <levasseur.cl@gmail.com>
2014-04-11 16:21:29 +0200

    added route for study order updates

--- routes/deckstudy.js
+++ routes/deckstudy.js
@@ -13,5 +13,21 @@ module.exports = function (app) {
       res.send(404);
     });
   });
+
+  app.put('/api/:username/:files?*', function (req, res, next) {
+    if (req.query.action !== 'updateStudyOrder') {
+      return next();
+    }
+
+    deckStudy.updateStudyOrder(2, req.body.fileId, req.body.studyOrderId).
+      then(function () {
+        res.send(204);
+      }).
+      catch(function (err) {
+        console.log(err);
+        res.send(400);
+      });
+  });
+
 };
 

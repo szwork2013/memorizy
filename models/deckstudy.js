c7f5e6cb341bde04ed1e0621cd26f92c0d548865
Carl Levasseur <levasseur.cl@gmail.com>
2014-04-11 16:19:06 +0200

    added updateStudyOrder function

--- models/deckstudy.js
+++ models/deckstudy.js
@@ -9,4 +9,22 @@ DeckStudy.prototype.updateStats = function (userId, stats) {
 
 };
 
+DeckStudy.prototype.updateStudyOrder = function (userId, fileId, orderId) {
+  if (typeof userId !== 'number') {
+    return q.reject('userId = ' + userId + ' (expected a number)');
+  }
+  if (typeof fileId !== 'number') {
+    return q.reject('fileId = ' + fileId + ' (expected a number)');	
+  }
+  if (typeof orderId !== 'number') {
+    return q.reject('orderId = ' + orderId + ' (expected a number)');	
+  }
+
+  return db.executePreparedStatement({
+    name : 'updateStudyOrder',
+    text : 'select update_study_order($1::INTEGER, $2::INTEGER, $3::INTEGER)',
+    values : [userId, fileId, orderId]
+  });
+};
+
 module.exports = singleton;

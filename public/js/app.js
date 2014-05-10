e4667f84bc96fa4abcb55692dd108731ae544e57
Carl Levasseur <levasseur.cl@gmail.com>
2014-05-10 19:53:42 +0200

    included new modules

--- public/js/app.js
+++ public/js/app.js
@@ -1,90 +1,99 @@
-angular.module('memorizy', [
-  'memorizy.navbar',
-  'memorizy.login',
-  'memorizy.filemanager',
-  'memorizy.filenavigation',
-  'memorizy.deckeditor',
-  'memorizy.deckstudy',
-  'memorizy.flashcard',
-  'memorizy.focus',
-  'memorizy.keyboard',
-  'memorizy.mouse',
-  'memorizy.contenteditable',
-  'memorizy.encodeURI',
-  'memorizy.input',
+(function () {
+  'use strict';
 
-  //'http-auth-interceptor',
+  angular.module('memorizy', [
+    'memorizy.navbar',
+    'memorizy.login',
+    'memorizy.filemanager',
+    'memorizy.filenavigation',
+    'memorizy.deckeditor',
+    'memorizy.deckstudy',
+    'memorizy.calendar',
+    'memorizy.flashcard',
+    'memorizy.focus',
+    'memorizy.keyboard',
+    'memorizy.mouse',
+    'memorizy.contenteditable',
+    'memorizy.encodeURI',
+    'memorizy.input',
 
-  /* Misc */
-  'ngRoute',
-  'ngStorage'
-]).
-  
-/**
- * intercept all http requests and manage user session 
- */
-factory('authInterceptor', function ($rootScope, $q, $localStorage) {
-  return {
-    request: function (config) {
-      config.headers = config.headers || {};
-      // send the session token at every request, if any
-      if ($localStorage.token) {
-        config.headers.Authorization = 'Bearer ' + $localStorage.token;
+    //'http-auth-interceptor',
+
+    /* Misc */
+    'ngRoute',
+    'ngStorage'
+  ]).
+    
+  /**
+   * intercept all http requests and manage user session 
+   */
+  factory('authInterceptor', function ($rootScope, $q, $localStorage) {
+    return {
+      request: function (config) {
+        config.headers = config.headers || {};
+        // send the session token at every request, if any
+        if ($localStorage.token) {
+          config.headers.Authorization = 'Bearer ' + $localStorage.token;
+        }
+        return config;
+      },
+
+      response: function (response) {
+        return response || $q.when(response);
+      },
+
+      responseError: function(rejection) {
+        // remove current token and user information
+        // if the server rejected the token, if they
+        // exists
+        if (rejection.status === 401) {
+          delete $localStorage.token;
+          delete $localStorage.user;
+
+          // the login form is displayed when
+          // the root scope does not have a property
+          // called 'user'
+          $rootScope.user = null;
+        }
+
+        return $q.reject(rejection);
       }
-      return config;
-    },
-
-    response: function (response) {
-      return response || $q.when(response);
-    },
-
-    responseError: function(rejection) {
-      // remove current token and user information
-      // if the server rejected the token, if they
-      // exists
-      if (rejection.status === 401) {
-        delete $localStorage.token;
-        delete $localStorage.user;
-
-        // the login form is displayed when
-        // the root scope does not have a property
-        // called 'user'
-        $rootScope.user = null;
-      }
-
-      return $q.reject(rejection);
-    }
-  };
-})
-.config(function ($routeProvider, $locationProvider, $httpProvider) {
-  $routeProvider.when('/', {
-    templateUrl: '/partials/index',
-    controller: 'IndexCtrl'
+    };
   })
-  .when('/login', {
-    templateUrl: '/partials/login',
-    controller: 'LoginCtrl'
+  .config(function ($routeProvider, $locationProvider, $httpProvider) {
+    $routeProvider.when('/', {
+      templateUrl: '/partials/index',
+      controller: 'IndexCtrl'
+    })
+    .when('/login', {
+      templateUrl: '/partials/login',
+      controller: 'LoginCtrl'
+    })
+    .when('/:username', {
+      templateUrl: '/partials/file-manager',
+      controller: 'FileManagerCtrl'
+    })
+    .when('/:username/calendar', {
+      templateUrl: '/partials/calendar',
+      controller: 'CalendarController'
+    })
+    .when('/:username/:subfolders*', {
+      templateUrl: '/partials/file-manager',
+      controller: 'FileManagerCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+    $locationProvider.html5Mode(true);
+
+    $httpProvider.interceptors.push('authInterceptor');
   })
-  .when('/:username', {
-    templateUrl: '/partials/file-manager',
-    controller: 'FileManagerCtrl'
-  })
-  .when('/:username/:subfolders*', {
-    templateUrl: '/partials/file-manager',
-    controller: 'FileManagerCtrl'
-  })
-  .otherwise({
-    redirectTo: '/'
+  .run(function ($rootScope, $location, $localStorage) {
+    // used for relative path 
+    $rootScope.$location = $location; 
+
+    // get insensitive information about the logged user
+    $rootScope.user = $localStorage.user;
   });
-
-  $locationProvider.html5Mode(true);
-
-  $httpProvider.interceptors.push('authInterceptor');
-})
-.run(function ($rootScope, $location, $localStorage) {
-  // used for relative path 
-  $rootScope.$location = $location; 
-
-  // get insensitive information about the logged user
-  $rootScope.user = $localStorage.user;
-});
+})();

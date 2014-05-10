24a1c0432e12db380d26447c6d4762d4dbc622e0
Carl Levasseur <levasseur.cl@gmail.com>
2014-05-10 19:59:11 +0200

    it is now possible to switch study method amid the current session

--- public/js/deckstudy/StudySession.js
+++ public/js/deckstudy/StudySession.js
@@ -15,17 +15,15 @@
     var that = this;
     $rootScope.$on('answered', function (event, answer) {
       var f = answer.flashcard;
-			console.log('f = ', f);
-      if (answer.correct === false) { f.status = -1; }
-      else if (f.status === -1 && answer.correct === true) { f.status = 1; }
-      else { f.status++; }
+      f.studied++;
 
-      if (f.status === 3) { 
-				console.log('remove index ' + that.subdeck.index);
+      if (f.status === 3 && that.subdeck) { 
+        console.log('remove flashcard because status == 3');
         // the flashcard is at 100%, we remove it from the subdeck
-        /*that.subdeck.indexes = */that.subdeck.indexes.splice(that.subdeck.index, 1);
+        that.subdeck.indexes.splice(that.subdeck.index, 1);
         // we add the next flashcard to the subdeck
         if (that.subdeck.end + 1 < that.deck.flashcards.length) {
+          console.log('add another flashcard');
           that.subdeck.end++; // increment the index of the subdeck's last flashcard 
           that.subdeck.indexes.push(that.subdeck.end);
         }
@@ -43,13 +41,12 @@
 				that.subdeck = {
 					index: 0,
 					begin: that.index,
-					end  : that.index + that.SUBDECK_MAX_LENGTH - 1,
+					end  : Math.min((that.index + that.SUBDECK_MAX_LENGTH - 1), that.deck.flashcards.length - 1),
 					indexes: []
 				};
-				for (var i = 0; i < that.SUBDECK_MAX_LENGTH; i++) {
-					that.subdeck.indexes[i] = that.index + i;
+				for (var i = 0; i <= that.subdeck.end - that.subdeck.begin; i++) {
+					that.subdeck.indexes[i] = that.subdeck.begin + i;
 				}
-				console.log('subdeck set to ', that.subdeck);
 			}
 			else {
 				console.log('set subdeck to null');
@@ -58,7 +55,7 @@
 		});
   }
 
-	StudySession.prototype.SUBDECK_MAX_LENGTH = 2;
+	StudySession.prototype.SUBDECK_MAX_LENGTH = 10;
 
   StudySession.prototype.configure = function (deck, config) {
     this.deck = deck;
@@ -70,12 +67,14 @@
       this.subdeck = {
         index: 0,
         begin: 0,
-        end  : this.SUBDECK_MAX_LENGTH - 1,
+        end  : Math.min(this.SUBDECK_MAX_LENGTH - 1, deck.flashcards.length - 1),
         indexes: []
       };
-			for (var i = 0; i < this.SUBDECK_MAX_LENGTH; i++) {
-				this.subdeck.indexes[i] = i;
-			}
+
+      console.log('subdeck end: ' + this.subdeck.end);
+      for (var i = 0; i <= this.subdeck.end - this.subdeck.begin; i++) {
+        this.subdeck.indexes[i] = this.subdeck.begin + i;
+      }
     }
     else {
       this.subdeck = null;
@@ -83,23 +82,37 @@
   };
 
   StudySession.prototype.addAnswer = function (flashcard, correct) {
-    this.stats.addAnswer(flashcard, correct);
+    if (correct === false) { 
+      flashcard.status = -1; 
+    }
+    else if (flashcard.status === -1 && correct === true) { 
+      flashcard.status = 1; 
+    }
+    else if (flashcard.status < 3) { 
+      flashcard.status++; 
+    }
+
+    this.stats.setStatus(flashcard, flashcard.status);
   };
 
   StudySession.prototype.next = function () {
     if (!this.subdeck) { // Classic method
       if (this.index < this.deck.flashcards.length - 1) { this.index++; }
-      else { this.$rootScope.$emit('end'); }
+      else { this.end(); }
     }
     else { // get 100 method
       if (this.subdeck.indexes.length === 0) {
-        this.$rootScope.$emit('end');
+        this.end();
       }
       else {
         this.subdeck.index = (this.subdeck.index + 1) % this.subdeck.indexes.length;
 				this.index = this.subdeck.indexes[this.subdeck.index];
       }
     }
+  };
+
+  StudySession.prototype.end = function () {
+    this.$rootScope.$emit('end');
   };
 
   angular.module('memorizy.deckstudy.StudySession', []). 

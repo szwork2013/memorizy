6cf1288cabb52f513b1cee5296953b99fa67703b
Carl Levasseur <levasseur.cl@gmail.com>
2014-07-21 19:24:48 +0200

    Add backup script

---
+++ backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BACKUP_PATH="${DIR}/backup.tar.gz"
+BRANCH=$(git branch | egrep ^[*\ ] | cut -f2 -d" ")
+
+# create the archive
+docker run --rm --volumes-from memorizy_db_data -v "$DIR":/backup ubuntu tar cvzf /backup/backup.tar.gz /var/lib/postgresql /etc/postgresql /var/log/postgresql
+
+# send to remote server
+git add -f "$BACKUP_PATH"
+git commit -m "backup" "$BACKUP_PATH" && git push origin "$BRANCH"

371cd2058d8319671ee4c1cbd4efcf3ef9e9d672
Carl Levasseur <levasseur.cl@gmail.com>
2014-07-30 00:08:11 +0200

    feat: add hash for registration

--- server/src/db/plpgsql/user/users.sql
+++ server/src/db/plpgsql/user/users.sql
@@ -1,8 +1,11 @@
 create or replace function create_user(_name text, _password text, _email text) returns record as $$
 declare
 	res			record;
+  salt    text := 'secret key !';
 	name_already_exists	boolean;
 	email_already_exists	boolean;
+  _user_id integer;
+  _hash    text;
 begin 
 	select exists (
 		select 1 from users
@@ -14,40 +17,33 @@ begin
 
 	
 	if not (name_already_exists or email_already_exists) then
-		insert into users (name, password, email, enabled)
-			values( _name, _password, _email, false);
-		res := (found, name_already_exists, email_already_exists); -- Arguments order must NOT be changed
-	else
-		res := (false, name_already_exists, email_already_exists);
+    with i as (
+      insert into users (name, password, email, hash, enabled)
+        values( _name, _password, _email, md5(salt || _name || _email), false)
+        returning id, hash
+    )
+    select id, hash into _user_id, _hash from i;
+
 	end if;
 
+  res := (found, _user_id, _hash, name_already_exists, email_already_exists); -- Arguments order must NOT be changed
 
 	return res;
-	--Impossible to say if the email already exists OR the name already exists
-	--insert into users (name, name_display, password, email, enabled)
-		--select distinct _real_name, _name_display, _password, _email, false
-		--from users
-		--where not exists (
-			--select 1 from users
-			--where name = _real_name or email = _email
-		--);
-
-	--return found;
 end;
 $$ language plpgsql;
 
-create or replace function enable_account(_user_id integer) returns void as $$
+create or replace function enable_account(_hash text) returns void as $$
 declare
 	_user_record	record;
 	_root_folder_id integer;
 begin
 	update users
 	set enabled = true
-	where id = _user_id
-	returning name into _user_record;
+	where hash = _hash
+	returning id, name into _user_record;
 
-	select create_file(_user_id, _user_record.name, 'folder', 0) into _root_folder_id;
-	perform create_file(_user_id, 'starred', 'folder', _root_folder_id);
+	select create_file(_user_record.id, _user_record.name, 'folder', 0) into _root_folder_id;
+	perform create_file(_user_record.id, 'starred', 'folder', _root_folder_id);
 end;
 $$ language plpgsql;
 

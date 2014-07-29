34a3aee8b9188509dc831b5cdc3a9e6262d5d082
Carl Levasseur <levasseur.cl@gmail.com>
2014-07-30 00:04:11 +0200

    chore: FileManager.getFiles() always return an array

--- client/app/filemanager/filemanager-controller.js
+++ client/app/filemanager/filemanager-controller.js
@@ -7,38 +7,22 @@
 
       $scope.refresh = function () {
         FileManager.getFiles().success(function (files) {
-          if (Object.prototype.toString.call(files) === '[object Array]') {
-            for (var i in files) {
-              if (file[i].type === 'deck') {
-                if (typeof $scope.decks === 'undefined') {
-                  $scope.decks = [];
-                }
-
-                $scope.decks.push(files[i]);
-                $scope.action = $routeParams.action;
-              }
-              else {
-                // If there are multiple files, it means we're studying several decks,
-                // so there types are always 'deck'
-                throw 'invalid file type';
-              }
-            }
-          }
-          else if (Object.prototype.toString.call(files) === '[object Object]') {
-            if (files.type === 'folder') {
-              $scope.folder = files;
-              $scope.action = $routeParams.action;
-            }
-            else if (files.type === 'deck') {
+          console.log('files = ', files);
+          for (var i in files) {
+            console.log(files[i]);
+            if (files[i].type === 'deck') {
               if (typeof $scope.decks === 'undefined') {
                 $scope.decks = [];
               }
-              $scope.decks.push(files);
+
+              $scope.decks.push(files[i]);
               $scope.action = $routeParams.action;
             }
-          }
-          else {
-            throw 'unknown data type';
+            else if (files[i].type === 'folder') {
+              $scope.folder = files[i];
+              $scope.action = $routeParams.action;
+            }
+            else { throw 'invalid file type'; }
           }
         })
         .error(function (err) {

cc26ce59c46e9dff0737becd2feb7210d7c0f375
Carl Levasseur <levasseur.cl@gmail.com>
2014-10-04 17:20:40 +0200

    Run data container in background

--- restore.sh
+++ restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run -it --name memorizy_db -v /home/carl/dev/memorizy:/db memorizy_db sh -c "tar xzvf /db/backup.tar.gz --no-overwrite-dir" && \
-	docker run -it --name memorizy_db_data --volumes-from memorizy_db ubuntu:14.04 /bin/bash  && \
+	docker run -it --name memorizy_db_data --volumes-from memorizy_db -d ubuntu:14.04 /bin/bash  && \
 	docker rm memorizy_db

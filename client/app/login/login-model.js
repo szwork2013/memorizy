b678e22d8d56966b60bb5a3f69077c4c0824a67a
Carl Levasseur <levasseur.cl@gmail.com>
2014-10-20 19:00:03 +0200

    Replace http by websockets

--- client/app/login/login-model.js
+++ client/app/login/login-model.js
@@ -1,28 +1,51 @@
-angular.module('memorizy.login.LoginModel', [])
-.provider('LoginModel', function () {
-  this.$get = ['$rootScope', '$http', '$location', '$localStorage',
-    function ($rootScope, $http, $location, $localStorage) {
-      return {
-        login: function (username, password) {
-          return $http.post('/login', { 
-            username: username,
-            password: password
-          })
-          .success(function (data) {
-            $rootScope.user = data.user; 
-            $localStorage.user = data.user;
-            $localStorage.token = data.token;
-          });
-        },
+(function () {
+  'use strict';
 
-        logout: function () {
-          $localStorage.$reset();
-          $location.path('/');
-          $rootScope.user = null;
-        }
-      };
-  }];
-});
+  function LoginModel($rootScope, socket, $location, $localStorage) {
+    this.$rootScope    = $rootScope;
+    this.socket        = socket;
+    this.$location     = $location;
+    this.$localStorage = $localStorage;
+
+    socket.on('login:loggedIn', function(data) {
+      $rootScope.user     = data.user;
+      $localStorage.user  = data.user;
+      $localStorage.token = data.token;
+    });
+
+    socket.on('login:loggedOut', function(data) {
+      $rootScope.user     = null;
+      $localStorage.user  = null;
+      $localStorage.token = null;
+    });
+  }
+
+  LoginModel.prototype.login = function(username, password) {
+    if (!username || !password) {
+      throw new Error('username and password cannot be falsy');
+    }
+
+    this.socket.emit('login:classic', {
+      login:    username,
+      password: password
+    });
+  };
+
+  //LoginModel.prototype.logout = function() {
+    //this.$localStorage.$reset();
+    //this.$location.path('/');
+    //this.$rootScope.user = null;
+  //};
+
+  angular.module('memorizy.login.LoginModel', [])
+  .service('LoginModel', [
+    '$rootScope',
+    'socketio',
+    '$location',
+    '$localStorage',
+    LoginModel
+  ]);
+})();
 
 
 
